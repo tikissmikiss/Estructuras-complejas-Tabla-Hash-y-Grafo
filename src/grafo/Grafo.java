87266b5da2c495ceece34414219ea9260819cbce
tikissmikiss <josehercepreciado@hotmail.com>
2021-01-08 16:33:30 +0100

    Grafo

--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -1,6 +1,18 @@
 package grafo;
 
 public class Grafo {
+    // El grafo debe cumplir los siguientes requisitos:
+    // ► Debe ser posible insertar cualquier tipo de objeto, aunque debe tener una
+    // clave numérica a modo de etiqueta que le represente.
+    // ► La estructura de datos de soporte puede ser tanto una lista como una matriz
+    // de adyacencia.
+    // ► Se deben implementar las operaciones básicas vistas en el temario de clase.
+    // ► Los alumnos deben asegurarse de que su código funciona correctamente
+    // mediante un conjunto de pruebas suficiente, que deben estar incluidas en una
+    // clase de prueba y deben mostrar los resultados convenientemente por consola,
+    // para verificar el funcionamiento. Se valorará positivamente que el grafo
+    // implementado se use en un PROBLEMA REAL.
+
     // Operaciones básicas
     // ► añadir(x) → nodo
     // ► borrar(x) → nodo y referencias
