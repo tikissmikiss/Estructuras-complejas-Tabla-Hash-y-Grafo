c411285e9c0d61b11e2518cdc30ba6e3faabb776
tikissmikiss <josehercepreciado@hotmail.com>
2021-01-08 16:09:49 +0100

    grafo

---
+++ src/grafo/Grafo.java
@@ -0,0 +1,18 @@
+package grafo;
+
+public class Grafo {
+    // Operaciones básicas
+    // ► añadir(x) → nodo
+    // ► borrar(x) → nodo y referencias
+    // ► adyacente(x, y) → bool si x, y son adyacentes
+    // ► vecinos(x) → nodos adyacentes a x
+    // ► añadir(x, y) → añadir arista que une x e y
+    // ► borrar(x, y) → borrar arista que une x e y
+    // ► obtenerValorNodo(x) → objeto encapsulado en el vértice
+    // ► establecerValorNodo(x, a) → asignar a como valor de x
+
+    // Operaciones básicas sobre grafos ponderados
+    // ► obtenerValorArco(x, y)
+    // ► establecerValorArco(x, y, a)
+
+}
