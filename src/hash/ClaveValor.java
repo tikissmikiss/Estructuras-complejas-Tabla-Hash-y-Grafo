75ceb260ec4b9fe56b957a7684b55986b8d37ae3
tikissmikiss <josehercepreciado@hotmail.com>
2021-01-11 19:12:51 +0100

    Revisado con Lorena

--- src/hash/ClaveValor.java
+++ src/hash/ClaveValor.java
@@ -15,4 +15,29 @@ class ClaveValor {
         return "[clave=" + clave + ", valor=" + valor + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((clave == null) ? 0 : clave.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ClaveValor other = (ClaveValor) obj;
+        if (clave == null) {
+            if (other.clave != null)
+                return false;
+        } else if (!clave.equals(other.clave))
+            return false;
+        return true;
+    }
+
 }
